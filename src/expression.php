62d04ba0295a8a53dd2681813575ecbc92154fd5
Remi Caput <git@mirari.fr>
2017-05-14 23:18:19 +0200

    Implement binary and unary operators.

--- src/expression.php
+++ src/expression.php
@@ -49,10 +49,40 @@ class BinaryExpression extends Expression
 		{
 			$functions = array
 			(
-				'+'	=> array
+				'%' => array
+				(
+					function ($lhs, $rhs) { return $lhs % $rhs; },
+					function ($lhs, $rhs) { return $lhs . '%' . $rhs; }
+				),
+				'&&' => array
+				(
+					function ($lhs, $rhs) { return $lhs && $rhs; },
+					function ($lhs, $rhs) { return $lhs . '&&' . $rhs; }
+				),
+				'*' => array
+				(
+					function ($lhs, $rhs) { return $lhs * $rhs; },
+					function ($lhs, $rhs) { return $lhs . '*' . $rhs; }
+				),
+				'+' => array
 				(
 					function ($lhs, $rhs) { return $lhs + $rhs; },
 					function ($lhs, $rhs) { return $lhs . '+' . $rhs; }
+				),
+				'-' => array
+				(
+					function ($lhs, $rhs) { return $lhs - $rhs; },
+					function ($lhs, $rhs) { return $lhs . '-' . $rhs; }
+				),
+				'/' => array
+				(
+					function ($lhs, $rhs) { return $lhs / $rhs; },
+					function ($lhs, $rhs) { return $lhs . '/' . $rhs; }
+				),
+				'||' => array
+				(
+					function ($lhs, $rhs) { return $lhs || $rhs; },
+					function ($lhs, $rhs) { return $lhs . '||' . $rhs; }
 				)
 			);
 		}
@@ -181,6 +211,42 @@ class UnaryExpression extends Expression
 {
 	public function __construct ($value, $op)
 	{
+		static $functions;
+
+		if (!isset ($functions))
+		{
+			$functions = array
+			(
+				'!' => array
+				(
+					function ($value) { return !$value; },
+					function ($value) { return '!' . $value; }
+				),
+				'+' => array
+				(
+					function ($value) { return $value; },
+					function ($value) { return '+' . $value; }
+				),
+				'-' => array
+				(
+					function ($value) { return -$value; },
+					function ($value) { return '-' . $value; }
+				),
+				'~' => array
+				(
+					function ($value) { return ~$value; },
+					function ($value) { return '~' . $value; }
+				)
+			);
+		}
+
+		if (!isset ($functions[$op]))
+			throw new \Exception ('unknown operator "' . $op . '"');
+
+		list ($evaluate, $generate) = $functions[$op];
+
+		$this->f_evaluate = $evaluate;
+		$this->f_generate = $generate;
 		$this->op = $op;
 		$this->value = $value;
 	}
@@ -192,12 +258,23 @@ class UnaryExpression extends Expression
 
 	public function generate (&$variables)
 	{
-		throw new \Exception ('not implemented');
+		$generate = $this->f_generate;
+
+		return $generate ($this->value->generate ($variables));
 	}
 
 	public function inject ($variables)
 	{
-		throw new \Exception ('not implemented');
+		$value = $this->value->inject ($variables);
+
+		if ($value->evaluate ($result))
+		{
+			$evaluate = $this->f_evaluate;
+
+			return new ConstantExpression ($evaluate ($result));
+		}
+
+		return new self ($value, $this->op);
 	}
 }
 
